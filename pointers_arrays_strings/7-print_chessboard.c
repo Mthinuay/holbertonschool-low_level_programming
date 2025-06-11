bb63ded9e24d7d0e686f1d148a5ba712aa70c273
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:16:48 +0000

    Implement print_chessboard to print an 8x8 chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,18 +2,18 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: 8x8 array representing the chessboard
+ * @a: pointer to an 8x8 array representing the board
  *
  * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int row, col;
+    int row, col;
 
-	for (row = 0; row < 8; row++)
-	{
-		for (col = 0; col < 8; col++)
-			_putchar(a[row][col]);
-		_putchar('\n');
-	}
+    for (row = 0; row < 8; row++)
+    {
+        for (col = 0; col < 8; col++)
+            _putchar(a[row][col]);
+        _putchar('\n');
+    }
 }

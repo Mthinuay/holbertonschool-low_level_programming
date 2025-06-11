53d4f8662290d5e30110f393a977f8957ad7fb4e
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 09:24:06 +0000

    Add 0-reset_to_98.c task for pointers_arrays_strings project

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value a pointer points to to 98
+ * @n: pointer to an int
+ *
+ * Description: This function takes a pointer to an int and
+ * updates the value it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

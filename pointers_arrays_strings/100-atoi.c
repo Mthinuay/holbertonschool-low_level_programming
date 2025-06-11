5f2dad00d8241ae590d03c85f7dfec0c8eafcbd0
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 10:03:53 +0000

    Implement _atoi function with full tests

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,23 +8,43 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, result = 0, found_digit = 0;
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+	int found_digit = 0;
 
-	while (s[i] != '\0')
+	/* Process all characters before the number */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
+		else if (s[i] == '+')
+			;
+		i++;
+	}
+
+	/* Convert digits */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		found_digit = 1;
+
+		/* Check for overflow */
+		if (num > (unsigned int)((2147483647 - (s[i] - '0')) / 10))
 		{
-			found_digit = 1;
-			result = result * 10 + (s[i] - '0');
+			if (sign == 1)
+				return (2147483647);
+			else
+				return (-2147483648);
 		}
-		else if (found_digit)
-			break;
+
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 
 	if (!found_digit)
 		return (0);
-	return (result * sign);
+
+	if (sign == -1 && num > 2147483648U)
+		return (-2147483648);
+
+	return ((int)(num * sign));
 }

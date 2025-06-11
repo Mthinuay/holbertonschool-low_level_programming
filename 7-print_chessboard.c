7ca40d4c9ddd07c7c4d0580738b0ddc3d14d3f93
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:45:48 +0000

    Fix 7-print_chessboard: single file with _putchar and main, Betty compliant

--- 7-print_chessboard.c
+++ 7-print_chessboard.c
@@ -1,4 +1,17 @@
-#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * This function writes a single character to standard output using write.
+ *
+ * Return: On success, 1. On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_chessboard - prints an 8x8 chessboard
@@ -19,4 +32,28 @@ void print_chessboard(char (*a)[8])
 			_putchar(a[row][col]);
 		_putchar('\n');
 	}
+}
+
+/**
+ * main - entry point to test print_chessboard
+ *
+ * This function initializes an 8x8 chessboard and calls print_chessboard to
+ * display it.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
+	return (0);
 }

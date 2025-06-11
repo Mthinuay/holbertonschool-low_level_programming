7d1535741a753630881fec7833bcb644bdc37356
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 13:00:00 +0000

    Fix 0-puts_recursion to use write only, no loops or static variables

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,16 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _puts_recursion - prints a string followed by a newline using recursion
- * @s: the string to print
- *
- * Return: void
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		write(1, "\n", 1);
 		return;
 	}
-	_putchar(*s);
+	write(1, s, 1);
 	_puts_recursion(s + 1);
 }

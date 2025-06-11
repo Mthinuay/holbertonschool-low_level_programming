8589a9025d694396e8fb32a897b6f3ef91271c03
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 10:12:04 +0000

    Fix 100-atoi.c with Betty style and correct INT_MIN handling

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,39 +8,37 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1;
-    unsigned int result = 0;
-    int found_digit = 0;
+	int i = 0;
+	int sign = 1;
+	unsigned int result = 0;
+	int found_digit = 0;
 
-    /* Count signs before number */
-    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign = -sign;
-        i++;
-    }
+	/* Count signs before number */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign = -sign;
+		i++;
+	}
 
-    /* Convert number */
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        found_digit = 1;
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+	/* Convert number */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		found_digit = 1;
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    if (!found_digit)
-        return (0);
+	if (!found_digit)
+		return (0);
 
-    /* If sign is negative and result is > INT_MAX + 1, it will wrap,
-       but since we can't use long or special values, we rely on unsigned int */
-    if (sign == -1 && result > 2147483648U)
-        return (0);
+	/* Check for INT_MIN */
+	if (sign == -1 && result == 2147483648U)
+		return (-2147483648);
 
-    if (sign == 1 && result > 2147483647U)
-        return (2147483647);
+	/* Cap values beyond INT_MAX */
+	if (sign == 1 && result > 2147483647U)
+		return (2147483647);
 
-    if (sign == -1 && result == 2147483648U)
-        return (-2147483648);
-
-    return ((int)(sign * result));
+	return ((int)(sign * result));
 }

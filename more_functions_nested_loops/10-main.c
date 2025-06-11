2346490836d09cde5263b5c4f3b8b078953c6aad
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 09:15:35 +0000

    Add 10-print_triangle.c: prints right-aligned triangle using '#'

---
+++ more_functions_nested_loops/10-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	print_triangle(-10);
+	print_triangle(3);
+	print_triangle(9);
+	print_triangle(98);
+	return (0);
+}

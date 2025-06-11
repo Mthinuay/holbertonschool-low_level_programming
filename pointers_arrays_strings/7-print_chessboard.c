7b5f60540851eb42ae38b56e5fe53e35c1a97002
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:33:19 +0000

    Implement 7-print_chessboard as single file with Betty style

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,54 @@
-#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_chessboard - prints the chessboard
- * @a: pointer to a 2D array of chars (8x8)
+ * @a: pointer to an 8x8 array of characters
  *
- * Description: prints an 8x8 chessboard where each row is printed
- * followed by a newline.
+ * Description: Prints an 8x8 chessboard where each element is a character.
+ * Each row is printed as 8 characters followed by a newline.
  *
  * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-    int row, col;
+	int row, col;
 
-    for (row = 0; row < 8; row++)
-    {
-        for (col = 0; col < 8; col++)
-            _putchar(a[row][col]);
-        _putchar('\n');
-    }
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
+}
+
+/**
+ * main - entry point to test print_chessboard
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
+	return (0);
 }

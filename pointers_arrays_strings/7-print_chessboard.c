d804f0fe9e8274fa0efd8f169d4facf365dc4b1a
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:35:27 +0000

    Fix 7-print_chessboard for checker: Betty style, separate files

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes a character to stdout
- * @c: the character to print
- *
- * Return: 1 on success, -1 on error
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_chessboard - prints the chessboard
+ * print_chessboard - prints an 8x8 chessboard
  * @a: pointer to an 8x8 array of characters
  *
- * Description: Prints an 8x8 chessboard where each element is a character.
+ * This function prints an 8x8 chessboard where each element is a character.
  * Each row is printed as 8 characters followed by a newline.
  *
  * Return: void
@@ -30,25 +19,4 @@ void print_chessboard(char (*a)[8])
 			_putchar(a[row][col]);
 		_putchar('\n');
 	}
-}
-
-/**
- * main - entry point to test print_chessboard
- *
- * Return: Always 0
- */
-int main(void)
-{
-	char board[8][8] = {
-		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-	};
-	print_chessboard(board);
-	return (0);
 }

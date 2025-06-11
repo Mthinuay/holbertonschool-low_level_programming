242e1ec1c20e1b5ec6c826e014dd3fb56ed4b455
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 07:48:32 +0000

    feat: add times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,21 +9,20 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (product < 10)
-					_putchar(' ');
-			}
+		_putchar('0');
 
-			if (product >= 10)
-			{
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = i * j;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
 				_putchar((product / 10) + '0');
-			}
+
 			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');

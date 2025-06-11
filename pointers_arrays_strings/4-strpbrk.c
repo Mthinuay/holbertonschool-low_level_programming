c341cba520addcee35111533baa028133cbb6a60
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:12:05 +0000

    Add 4-strpbrk.c: search string for any of a set of bytes

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,10 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: the string to search
- * @accept: the bytes to search for
+ * @s: the string to be scanned
+ * @accept: the string containing the bytes to match
  *
- * Return: pointer to the byte in s that matches one of the bytes in accept,
+ * Return: pointer to the byte in s that matches one in accept,
  * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
@@ -14,12 +14,13 @@ char *_strpbrk(char *s, char *accept)
 
 	while (*s)
 	{
-		for (i = 0; accept[i]; i++)
+		for (i = 0; accept[i] != '\0'; i++)
 		{
 			if (*s == accept[i])
 				return (s);
 		}
 		s++;
 	}
-	return (NULL);
+
+	return (0);
 }

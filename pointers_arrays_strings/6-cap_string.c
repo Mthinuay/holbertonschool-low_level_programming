d26ea3f411e475dc1f9cd4b5ee3d6b336d9915b6
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 10:42:02 +0000

    Fix 6-cap_string.c by moving is_separator outside cap_string to avoid nested functions

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * is_separator - checks if a character is a separator
+ * @c: the character to check
+ *
+ * Return: 1 if c is a separator, 0 otherwise
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * cap_string - capitalizes all words of a string
  * @s: string to modify
  *
@@ -9,20 +28,6 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-
-	/* Helper function to check if char is a separator */
-	int is_separator(char c)
-	{
-		char separators[] = " \t\n,;.!?\"(){}";
-		int j;
-
-		for (j = 0; separators[j] != '\0'; j++)
-		{
-			if (c == separators[j])
-				return (1);
-		}
-		return (0);
-	}
 
 	/* Capitalize first char if lowercase */
 	if (s[0] >= 'a' && s[0] <= 'z')

d5ab791b91009ab7949574e28313ebdb8891d11b
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 12:18:29 +0000

    Implement print_chessboard following Betty style and allowed functions

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,18 +2,21 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: pointer to an 8x8 array representing the board
+ * @a: pointer to a 2D array of chars (8x8)
+ *
+ * Description: prints an 8x8 chessboard where each row is printed
+ * followed by a newline.
  *
  * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-    int row, col;
+	int row, col;
 
-    for (row = 0; row < 8; row++)
-    {
-        for (col = 0; col < 8; col++)
-            _putchar(a[row][col]);
-        _putchar('\n');
-    }
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+			_putchar(a[row][col]);
+		_putchar('\n');
+	}
 }

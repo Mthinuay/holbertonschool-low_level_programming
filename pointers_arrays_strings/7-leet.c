663b4a06798f6d1ea4d9d2c67122d13f871d3868
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 10:45:17 +0000

    Fix indentation and implement 7-leet.c to encode string into 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,28 +4,28 @@
  * leet - encodes a string into 1337
  * @s: string to encode
  *
- * Return: pointer to encoded string
+ * Return: pointer to the encoded string
  */
 char *leet(char *s)
 {
-    int i, j;
-    char *letters = "aAeEoOtTlL";
-    char *numbers = "4433007711";
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
 
-    i = 0;
-    while (s[i] != '\0')
-    {
-        j = 0;
-        while (letters[j] != '\0')
-        {
-            if (s[i] == letters[j])
-            {
-                s[i] = numbers[j];
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
-    return (s);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
 }

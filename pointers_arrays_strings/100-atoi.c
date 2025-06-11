4ef698b3d9d0b24640b55996f383bbf09edcd160
root <root@ip-10-42-23-177.ec2.internal>
2025-06-11 10:09:57 +0000

    Fix _atoi to handle INT_MIN correctly and add test cases

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,32 +4,43 @@
  * _atoi - converts a string to an integer
  * @s: the string to convert
  *
- * Return: integer value parsed from the string, or 0 if none found
+ * Return: the integer value of the string, or 0 if no numbers found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num = 0, started = 0;
+    int i = 0, sign = 1;
+    unsigned int result = 0;
+    int found_digit = 0;
 
-	while (s[i])
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] == '+')
-			;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
+    /* Count signs before number */
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign = -sign;
+        i++;
+    }
 
-			/* check overflow without using long or hardcoded limits */
-			if (num > (num * 10 + (s[i] - '0')) / 10)
-				return (sign == 1 ? num : -num);
+    /* Convert number */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        found_digit = 1;
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-			num = num * 10 + (s[i] - '0');
-		}
-		else if (started)
-			break;
-		i++;
-	}
+    if (!found_digit)
+        return (0);
 
-	return (sign * num);
+    /* If sign is negative and result is > INT_MAX + 1, it will wrap,
+       but since we can't use long or special values, we rely on unsigned int */
+    if (sign == -1 && result > 2147483648U)
+        return (0);
+
+    if (sign == 1 && result > 2147483647U)
+        return (2147483647);
+
+    if (sign == -1 && result == 2147483648U)
+        return (-2147483648);
+
+    return ((int)(sign * result));
 }

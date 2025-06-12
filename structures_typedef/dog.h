052bbd56ea5f4b198f6f908c612f1fd5040ea0d3
root <root@ip-10-42-77-91.ec2.internal>
2025-06-12 09:59:30 +0000

    Fix: Betty-compliant struct dog definition in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,13 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - structure that stores dog information
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - A dog's basic information
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
  *
- * Description: Defines a new struct type that stores a dog's
- * name, age, and owner.
+ * Description: A struct to store a dog's name, age, and owner.
  */
 struct dog
 {
@@ -18,20 +17,4 @@ struct dog
 };
 
 #endif /* DOG_H */
-#ifndef DOG_H
-#define DOG_H
 
-/**
- * struct dog - A dog's basic info
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- */
-struct dog
-{
-    char *name;
-    float age;
-    char *owner;
-};
-
-#endif /* DOG_H */

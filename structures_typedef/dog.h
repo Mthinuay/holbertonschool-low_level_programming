74c956066ff88224a39eb0c7a9c63bd0d5884b91
root <root@ip-10-42-77-91.ec2.internal>
2025-06-12 09:50:48 +0000

    Task 0: Define struct dog in Betty-compliant style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,6 +2,26 @@
 #define DOG_H
 
 /**
+ * struct dog - structure that stores dog information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Defines a new struct type that stores a dog's
+ * name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+#ifndef DOG_H
+#define DOG_H
+
+/**
  * struct dog - A dog's basic info
  * @name: The name of the dog
  * @age: The age of the dog

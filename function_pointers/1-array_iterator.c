de84d6e2ddcb2c5dbc4d2e969783e30957bd4419
root <root@ip-10-42-184-91.ec2.internal>
2025-06-12 13:16:14 +0000

    Fix Betty style in 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,20 +1,20 @@
+#include "function_pointers.h"
 #include <stddef.h>
 
 /**
- * array_iterator - executes a function on each element of an array
- * @array: the array to iterate over
- * @size: the size of the array
- * @action: the function to execute on each element
+ * array_iterator - Executes a function on each element of an array
+ * @array: Pointer to the array
+ * @size: Size of the array
+ * @action: Pointer to the function to be executed
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i;
+	size_t i;
 
-    if (array == NULL || action == NULL)
-        return;
+	if (array == NULL || action == NULL)
+		return;
 
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);
-    }
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
+

37dd55ee4862d4638a89e3f05d2fcfcf375d9434
Nkosinathi Mazibuko <107320866+Mthinuay@users.noreply.github.com>
2025-06-12 17:12:50 +0200

    Update 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,11 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (index != (n - 1) && separator != NULL)
+		if (separator != NULL && index != (n - 1))
 			printf("%s", separator);
 	}
 
 	printf("\n");
-
 	va_end(nums);
 }

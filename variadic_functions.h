da59bc5f669c0884cd6e653a7a6e7e415fe9b0ae
Nkosinathi Mazibuko <107320866+Mthinuay@users.noreply.github.com>
2025-06-12 16:49:50 +0200

    Update variadic_functions.h

--- variadic_functions.h
+++ variadic_functions.h
@@ -3,13 +3,9 @@
 
 #include <stdarg.h>
 
-/**
- * sum_them_all - Returns the sum of all its parameters.
- * @n: Number of parameters passed to the function.
- *
- * Return: Sum of all parameters. 0 if n == 0.
- */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif /* VARIADIC_FUNCTIONS_H */
-
+#endif

b5cf30ceffa0aef6387fa6890969bbfa6245d1f1
root <root@ip-10-42-184-91.ec2.internal>
2025-06-12 14:46:54 +0000

    Add print_numbers function and update variadic_functions.h

--- variadic_functions.h
+++ variadic_functions.h
@@ -3,6 +3,13 @@
 
 #include <stdarg.h>
 
+/**
+ * sum_them_all - Returns the sum of all its parameters.
+ * @n: Number of parameters passed to the function.
+ *
+ * Return: Sum of all parameters. 0 if n == 0.
+ */
 int sum_them_all(const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+

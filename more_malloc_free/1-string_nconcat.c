d222adf23256fa2eedf93f0c414ab3be9db433ba
root <root@ip-10-42-77-91.ec2.internal>
2025-06-12 09:02:35 +0000

    Fix 1-string_nconcat: remove strlen, ensure Betty compliance

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - Concatenates two strings, using at most n bytes of s2
- * @s1: First string
- * @s2: Second string
- * @n: Maximum number of bytes from s2 to concatenate
+ * string_nconcat - Concatenates two strings using at most n bytes of s2.
+ * @s1: First string.
+ * @s2: Second string.
+ * @n: Maximum number of bytes from s2 to concatenate.
  *
- * Return: Pointer to newly allocated concatenated string, or NULL on failure
+ * Return: Pointer to newly allocated string, or NULL on failure.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concat;
-    unsigned int len1, len2, total_len, i;
+	char *concat;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+	while (s1[len1])
+		len1++;
 
-    if (n >= len2)
-        n = len2;
+	while (s2[len2])
+		len2++;
 
-    total_len = len1 + n;
+	if (n > len2)
+		n = len2;
 
-    concat = malloc(total_len + 1); /* +1 for null terminator */
-    if (concat == NULL)
-        return (NULL);
+	concat = malloc(sizeof(char) * (len1 + n + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-    for (i = 0; i < n; i++)
-        concat[len1 + i] = s2[i];
+	for (j = 0; j < n; j++)
+		concat[i + j] = s2[j];
 
-    concat[total_len] = '\0';
+	concat[i + j] = '\0';
 
-    return (concat);
+	return (concat);
 }

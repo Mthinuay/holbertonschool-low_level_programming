75b266bb08bf23d59175a34ac7ec5499f74b7bfc
root <root@ip-10-42-77-91.ec2.internal>
2025-06-12 11:42:14 +0000

    Fix Betty style for 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,15 +2,18 @@
 #include "dog.h"
 
 /**
- * free_dog - Frees memory allocated to a dog
- * @d: pointer to the dog to free
+ * free_dog - Frees memory allocated for a dog_t struct
+ * @d: Pointer to the dog to free
+ *
+ * Description: Frees name and owner strings, then the struct itself.
  */
 void free_dog(dog_t *d)
 {
-    if (d)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
+
